401fece9179733bb96b5f2d8eba31f73a6f3a8b5
idris enes <i.enes.2106@gmail.com>
2026-03-30 21:48:59 +0300

    fix: hardcode resend api key for serverless deployment

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,7 +1,7 @@
 import { Resend } from "resend";
 import { NextRequest, NextResponse } from "next/server";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("re_roa4wnMx_5sKis6RLdEK2t26NGV5F1xeX");
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
